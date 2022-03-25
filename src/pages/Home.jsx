aee796c70980c0683424903c30fec9679c3e0f74
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-25 00:58:56 -0500

    se crea card en el hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 
 const Home = () => {
-  return (
-    <div className="hero">
-      <div class="card bg-dark text-white">
-        <img src="https://images.pexels.com/photos/5210380/pexels-photo-5210380.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" class="card-img" alt="background" />
-        <div class="card-img-overlay">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-          </p>
-          <p class="card-text">Last updated 3 mins ago</p>
+    return (
+        <div className="hero">
+            <div class="card bg-dark text-white">
+                <img src="https://images.pexels.com/photos/5210380/pexels-photo-5210380.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" class="card-img" alt="background" height="550px"/>
+            <div class="card-img-overlay">
+
+            <div className="container">
+                <h5 class="card-title display-3 fw-bolder mb-0">STYLE, SELF-LOVE, INSPIRATION</h5>
+                <p class="card-text">
+                This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+            </p>
+
+            </div>
         </div>
-      </div>
-    </div>
-  );
+        </div>
+        </div>
+        );
 };
 
 export {Home};
