ee0ec1471b010f48f187e89b0cbe0bb70582245a
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-25 00:25:02 -0500

    se instala dependencia react-icons

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {SliderData} from './SliderData';
 
 const ImageSlider = () => {
+
+    const [current, setCurrent] = useState(0);
+    const length = slides.length;
 
     return (
         <>
