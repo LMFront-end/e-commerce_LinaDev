8338baa0c1a58d64d27ae75474f5310c719c79d7
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-24 22:42:56 -0500

    se elimina el form search del nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -33,22 +33,19 @@ const NavBar = () => {
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="#">Products</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
                     </li>
 
                 </ul>
 
-                    <form class="d-flex">
-                        <input
-                            class="form-control me-2"
-                            type="search"
-                            placeholder="Search"
-                            aria-label="Search"
-                        />
-                        <button class="btn btn-outline-success" type="submit">
-                            Search
-                        </button>
-                    </form>
                 </div>
             </div>
         </nav>
