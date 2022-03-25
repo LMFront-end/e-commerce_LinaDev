5100d49aa647088c987236fd6655f43228e4c70b
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-25 00:51:47 -0500

    se elimina la carpeta images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import { ImageSlider } from '../components/ImageSlider'
-
-
+import React from 'react';
 
 const Home = () => {
-    return (
-        <div>
-        <ImageSlider />
-        <h4>Home</h4>
+  return (
+    <div className="hero">
+      <div class="card bg-dark text-white">
+        <img src="https://images.pexels.com/photos/5210380/pexels-photo-5210380.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" class="card-img" alt="background" />
+        <div class="card-img-overlay">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+          </p>
+          <p class="card-text">Last updated 3 mins ago</p>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export {Home}
+export {Home};
