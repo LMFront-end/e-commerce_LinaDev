996fc9b0fc980d34660e01496eba84ac2d9ffb96
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-26 11:52:34 -0500

    se agrega nueva page Products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,11 @@
 import React, {useState, useEffect} from 'react'
 
 const Products = () => {
+
+    const [data, setData] = useState([]);
+    const [filter, setFilter] = useState(data);
+    const [loading, setLoading] = useState(false);
+
     return (
         <div>Products</div>
     )
