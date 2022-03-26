9e309a0ebecce37e4808cf9ae680ee364aa34b4d
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-26 16:58:17 -0500

    se elimina la carpeta icons

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,23 +4,18 @@ import Skeleton from 'react-loading-skeleton'
 const Loading = () => {
     return (
         <>
-        <div clasName="d-flex justify-content-center col-md-3">
+        <div clasName=" col-md-6">
             <Skeleton height={400}/>
         </div>
-        <div clasName="d-flex justify-content-center col-md-3">
+
+        <div clasName="col-md-6" style={{lineHeight:2}}>
             <Skeleton height={50} width={350}/>
-        </div>
-        <div clasName="d-flex justify-content-center col-md-3">
             <Skeleton height={75}/>
-        </div>
-        <div clasName="d-flex justify-content-center col-md-3">
             <Skeleton height={25} width={150}/>
-        </div>
-        <div clasName="d-flex justify-content-center col-md-3">
             <Skeleton height={50} />
-        </div>
-        <div clasName="d-flex justify-content-center col-md-3">
             <Skeleton height={150}/>
+            <Skeleton height={50} width={100}/>
+            <Skeleton height={50} width={100} style={{marginLeft:6}}/>
         </div>
         </>
 
