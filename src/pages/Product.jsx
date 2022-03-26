bdef00f6de5f8d4386fd6e28f54c458885deca6f
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-26 15:33:11 -0500

    se crea Page Product

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {useParams} from 'react-router';
 
 const Product = () => {
+
+    const {id} = useParams;
+    const [product, setProduct] = useState([]);
+
     return (
         <div>Product</div>
     )
